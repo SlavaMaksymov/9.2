6c1edabfe20a39a9725a4a3f595aacb68446f392
SlavaMaksymov <slaventiy.maksimov@gmail.com>
2024-07-22 12:43:02 +0300

    Category.cs

--- Entity/Category.cs
+++ Entity/Category.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,16 +9,28 @@ namespace _9._2.Entity
 {
     internal class Category
     {
-        int id;
-        string name;
+        int id { get; set; }
+        string name { get; set; }
 
-        List<Good> products;
+        public static List<Good> products { get; set; }
 
         public Category (string name) 
         {
             id = (new Random()).Next(1, 100);
             this.name = name;
             products = new List<Good>();
+            
         }
+
+        public void ShowGoods(List<Good> elements) {
+            for (int i=0; i < products.Count; i++)
+                Console.WriteLine(products[i].GetName(products[i]));
+        }
+
+        public void AddGoods(Good good) {
+            
+            products.Add(good);
+        }
+
     }
 }
