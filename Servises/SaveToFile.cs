0ceb40cc60d0907bad66d2445fde9ef73143fdc0
SlavaMaksymov <slaventiy.maksimov@gmail.com>
2024-07-22 12:46:02 +0300

    SaveToFile.cs

--- Servises/SaveToFile.cs
+++ Servises/SaveToFile.cs
@@ -1,9 +1,12 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
+using _9._2.Entity;
 
 namespace _9._2.Servises
 {
@@ -14,6 +17,39 @@ namespace _9._2.Servises
         public SaveToFile(string path) 
         {
             this .path = path;
+        }
+
+        public void save(Category category) 
+        {
+            using (StreamWriter streamWriter = new StreamWriter(this.path, true))
+            {
+                string json = JsonSerializer.Serialize(category);
+                streamWriter.WriteLine(json);
+            }
+        
+        }
+
+        public void save(Good good)
+        {
+            using (StreamWriter streamWriter = new StreamWriter(this.path, true))
+            {
+                string json = JsonSerializer.Serialize(good);
+                streamWriter.WriteLine(json);
+            }
+
+        }
+
+
+        public Category load()
+
+        {
+            Category category = null;
+            using (StreamReader streamReader = new StreamReader(this.path))
+            {
+                string json = streamReader.ReadLine();
+                //category = JsonSerializer.Deserialize<Category>(json);
+            }
+            return category;
         }
     }
 }
